0b6d7a03cb3b3e71f5ec6bfd89ff178e609c81ff
léon h <hello@leonh.fr>
2019-03-24 11:26:17 +0100

    [BUG] Optional zone (#14)

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,14 @@
+// Packages.
+import * as turfHelpers from '@turf/helpers';
+
 // Definition.
 export type IsSafe = {
   isSafe: boolean;
   errMsg: string;
 };
+
+export type BoundingBox = turfHelpers.BBox;
+
+export type MultiPolygon = turfHelpers.Feature<
+  turfHelpers.Polygon | turfHelpers.MultiPolygon
+>;
